a54cb96b26ec55f967b3472c3de9a298ae2dee42
David Elizondo <daveligu@gmail.com>
2019-08-19 15:37:55 -0600

    update

--- movie_search_web/lib/src/components/search_body/search_body_component.dart
+++ movie_search_web/lib/src/components/search_body/search_body_component.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 
 import 'package:angular/angular.dart';
 import 'package:angular/core.dart';
+import 'package:angular_components/angular_components.dart';
 import 'package:angular_components/utils/browser/window/module.dart';
 import 'package:movie_search_core/movie_search_core.dart';
 import 'package:movie_search_web/src/components/search_results/search_result_component.dart';
@@ -10,7 +11,11 @@ import 'package:movie_search_web/src/model/search_data.dart';
 @Component(
     selector: "search-body",
     templateUrl: "search_body_component.html",
-    directives: [coreDirectives, SearchResult])
+    directives: [
+      coreDirectives,
+      SearchResult,
+      MaterialSpinnerComponent,
+    ])
 class SearchBody implements AfterViewInit {
   final SearchData _searchData;
 
